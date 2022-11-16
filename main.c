3fb5115fad540b3a8f90a3e5c84e0a44b6e464ef
BuildTools <unconfigured@null.spigotmc.org>
2022-11-16 17:15:26 +1100

    added k1ngw1ng's code

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 //AMJ was here
 // FG6 says hello
 //Nibroc poked buttons
+//K1ngw1ng Says Hola
 
 
 //variables
@@ -25,5 +26,21 @@ int main()
           xbox = 0;
         }
     }
+    char c;
+    int lowercase_vowel, uppercase_vowel;
+    printf("\nEnter an alphabet: ");
+    scanf("%c", &c);
+
+    // evaluates to 1 if variable c is a lowercase vowel
+    lowercase_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+    // evaluates to 1 if variable c is a uppercase vowel
+    uppercase_vowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+    // evaluates to 1 (true) if c is a vowel
+    if (lowercase_vowel || uppercase_vowel)
+        printf("%c is a vowel.", c);
+    else
+        printf("%c is a consonant.", c);
     return 0;
 }
